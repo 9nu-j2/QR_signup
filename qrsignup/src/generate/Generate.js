854ba4e36a225f4f3d2fb3a7f3b67cb1e312df05
2-jun9 <jungu269@gmail.com>
2022-05-17 16:00:41 +0900

    프린트 버튼 및 이미지 저장 버튼 추가

--- qrsignup/src/generate/Generate.js
+++ qrsignup/src/generate/Generate.js
@@ -4,10 +4,33 @@ import QRCode from 'qrcode.react';
 import { pin } from '../register/Register';
 import { storeName } from '../register/Register';
 
+import { useRef } from 'react';
+import { ReactToPrint, useReactToPrint } from 'react-to-print';
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
+
 function Generate() {
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: ()=>componentRef.current,
+  });
+
+  const onDownloadBtn = () => {
+    const card = componentRef.current;
+    domtoimage
+      .toBlob(card)
+      .then((blob) => {
+        saveAs(blob, 'card.png');
+      });
+  };
+
   return (
     <div className='All'>
-      <div className="Div1">
+      <div className="Buttons">
+          <button className="button-16" onClick={handlePrint}>프린트하기</button>
+          <button className="button-16" onClick={onDownloadBtn}>이미지 저장하기</button>
+        </div>
+      <div className="Div1" ref={componentRef}>
       <img src={poster}></img>
         <div className='Upper'></div>
         <div className='Titlepart'><div className="Store">{storeName}</div></div>
--- qrsignup/src/wifi/Wifi.js
+++ qrsignup/src/wifi/Wifi.js
@@ -1,8 +1,11 @@
 import './Wifi.scss';
 import qrwifi from '../img/QR_WiFI.png';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Link } from "react-scroll";
 import QRCode from 'qrcode.react';
+import { ReactToPrint, useReactToPrint } from 'react-to-print';
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
 
 
 function Wifi() {
@@ -64,38 +67,44 @@ function Wifi() {
 }
 
 function Modal(props) {
-  const printPage = (e) => {
-    const html = document.querySelector('html');
-    const printSection = document.querySelector('.Di2').innerHTML;
-    const printDiv = document.createElement("DIV");
-    html.appendChild(printDiv);
-    printDiv.innerHTML = printSection;
-    document.body.style.display = 'none';
-    window.print();
-    document.body.style.display = 'block';
-    printDiv.style.display = 'none';
-  }
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: ()=>componentRef.current,
+  });
+
+  const onDownloadBtn = () => {
+    const card = componentRef.current;
+    domtoimage
+      .toBlob(card)
+      .then((blob) => {
+        saveAs(blob, 'card.png');
+      });
+  };
+
   if(props.모달===true){
     return (
       <div className="Back">
-      <button className="button-16" onClick={printPage}>프린트하기</button>
-      <div className='Di2'>
-        <div className="Center" id="go">
-          <img src={qrwifi}></img>
-          <div className="P1"></div>
-          <div className="P2">
-            <h1>{props.inputs.storename}</h1>
-          </div>
-          <div className="P3"></div>
-          <div className="P4">
-          <QRCode value={"WIFI:S:" + props.inputs.ssid + ";T:WPA;P:" + props.inputs.pw + ";"} renderAs="canvas" size={325}/>
-          </div>
-          <div className="P5">
-            <p>SSID : {props.inputs.ssid}</p>
-            <p>비밀번호 : {props.inputs.pw}</p>
+        <div className="Buttons">
+          <button className="button-16" onClick={handlePrint}>프린트하기</button>
+          <button className="button-16" onClick={onDownloadBtn}>이미지 저장하기</button>
+        </div>
+        <div className='Di2'>
+          <div className="Center" ref={componentRef} id="go">
+            <img src={qrwifi}></img>
+            <div className="P1"></div>
+            <div className="P2">
+              <h1>{props.inputs.storename}</h1>
+            </div>
+            <div className="P3"></div>
+            <div className="P4">
+            <QRCode value={"WIFI:S:" + props.inputs.ssid + ";T:WPA;P:" + props.inputs.pw + ";"} renderAs="canvas" size={325}/>
+            </div>
+            <div className="P5">
+              <p>SSID : {props.inputs.ssid}</p>
+              <p>비밀번호 : {props.inputs.pw}</p>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     )
   }
