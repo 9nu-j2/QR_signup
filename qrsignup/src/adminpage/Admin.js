8e42d87f99899c35593213411fabd37118735fdb
2-jun9 <jungu269@gmail.com>
2022-06-14 11:03:27 +0900

    관리자 관리 페이지 개발중

--- qrsignup/src/adminpage/Admin.js
+++ qrsignup/src/adminpage/Admin.js
@@ -3,9 +3,9 @@ import './Admin.css';
 import logo from '../img/kt.png'
 
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-import { child, get, set, ref } from "firebase/database";
+import { child, get, ref } from "firebase/database";
 import { database } from "../firebase";
 
 function Admin() {
@@ -13,12 +13,11 @@ function Admin() {
   const [inputs, setInputs] = useState({
     id: '',
     password: '',
-    area: ''
   }); // 멀티 인풋 값을 관리하는 state
 
   const dbRef = ref(database);
 
-  const { id, password, area } = inputs;
+  const { id, password } = inputs;
 
   function Button() {
     return(<button onClick={onClick}>로그인</button>);
@@ -42,16 +41,16 @@ function Admin() {
 
   };
 
-  const handleChange = (e) => {
-    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
-    setInputs({
-      ...inputs, // 기존의 input 객체를 복사한 뒤, 하드카피
-      [name]: value // name 키를 가진 값을 value 로 설정
-    });
-	};
-
   const onClick = () => {
-    navigate("waiting/admin/manage");
+    get(child(dbRef, 'admin/' + `${inputs.id}`)).then((snapshot)=>{
+      if (snapshot.exists()) { 
+        navigate("/waiting/admin/manage");
+      } else {
+        console.log("안됨");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
   }
 
   return (
