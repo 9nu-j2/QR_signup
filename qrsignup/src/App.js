972c8b93c958c964a964b7f9e4115bc6728b4369
2-jun9 <jungu269@gmail.com>
2022-06-15 17:16:40 +0900

    로그인 유지를 위한 세션기능 추가

--- qrsignup/src/App.js
+++ qrsignup/src/App.js
@@ -4,20 +4,35 @@ import Generate from './generate/Generate.js';
 import Wifi from './wifi/Wifi.js';
 import Master from './main/Main.js';
 import Admin from './adminpage/Admin.js';
-import Approve from './approve/Approve.js';
+import Select from './select/Select.js';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function App() {
+  console.log(sessionStorage.getItem('user_id'));
+  const [sessionCheck, sessionChange] = useState(false)
+  useEffect(() => {
+    if(sessionStorage.getItem('user_id') === null){
+    // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 없다면
+      console.log('isLogin ?? :: ', sessionCheck)
+    } else {
+    // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 있다면
+    // 로그인 상태 변경
+      sessionChange(true)
+      console.log('isLogin ?? :: ', sessionCheck)
+    }
+  })
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Master />} />
           <Route path='/wifi' element={<Wifi />}/>
-          <Route path='/waiting' element={<Register />} />
-          <Route path='/waiting/qr' element={<Generate />} />
           <Route path='/waiting/admin' element={<Admin />} />
-          <Route path='/waiting/admin/manage' element={<Approve />} />
+          <Route path='/waiting' element={<Register isLogin={sessionCheck}/>} />
+          <Route path='/waiting/qr' element={<Generate isLogin={sessionCheck}/>} />
+          <Route path='/waiting/select' element={<Select isLogin={sessionCheck}/>} />
         </Routes>
       </BrowserRouter>
     </div>
