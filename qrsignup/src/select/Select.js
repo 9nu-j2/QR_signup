1203c127268b0af2b4a96e1e259bbc081bad8dcf
2-jun9 <jungu269@gmail.com>
2022-06-17 16:29:06 +0900

    관리자 페이지 1 디자인 일부 적용

--- qrsignup/src/select/Select.js
+++ qrsignup/src/select/Select.js
@@ -7,19 +7,19 @@ import { useEffect, useState } from 'react';
 
 function Select(props) {
   const navigate = useNavigate();
-  const isLogin = props.isLogin
 
   useEffect(()=>{
-    if(isLogin === false) {
+    if(props.isLogin === false) {
       navigate("/waiting/admin");
+      console.log(props.isLogin);
     }
   }) // 세션 유지 여부 확인, 접근 제한
   
   const onClickLogout = ()=>{
     // sessionStorage 에 user_id 로 저장되어있는 아이템을 삭제한다.
-    sessionStorage.removeItem('user_id')
+    sessionStorage.removeItem('user_id');
     // App 으로 이동(새로고침)
-    navigate("/waiting/admin");
+    document.location.href = '/waiting/admin';
   } // 로그아웃 기능
 
   return (
@@ -28,13 +28,14 @@ function Select(props) {
         <img src={logo}></img>
       </div>
       <div className="Half">
+        <div className="IconBox"></div>
+        <p>관리자 정보(아이디)</p>
+        <p>관리자 소속</p>
         <button onClick={onClickLogout}>로그아웃</button>
       </div>
-      <div className="Half">
-        <div className="AdminSelect">
-          <button><div className="AdminMenu" onClick={()=>navigate("/waiting")}>신규 고객 등록하기</div></button>
-          <button><div className="AdminMenu" onClick={()=>navigate("/waiting/monitor")}>승인 요청 확인하기</div></button>
-        </div>
+      <div className="AdminSelect">
+        <button><div className="AdminMenu" onClick={()=>navigate("/waiting")}>신규 고객 등록하기</div></button>
+        <button><div className="AdminMenu" onClick={()=>navigate("/waiting/monitor")}>승인 요청 확인하기</div></button>
       </div>
     </div>
   );
