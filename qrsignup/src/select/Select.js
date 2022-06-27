7bcd57c56e80f5249c17ec6e33ee1b55f4d06318
2-jun9 <jungu269@gmail.com>
2022-06-27 17:31:36 +0900

    버그 수정

--- qrsignup/src/select/Select.js
+++ qrsignup/src/select/Select.js
@@ -38,7 +38,7 @@ function Select(props) {
     // sessionStorage 에 user_id 로 저장되어있는 아이템을 삭제한다.
     sessionStorage.removeItem("user_id");
     // App 으로 이동(새로고침)
-    navigate("/waiting");
+    document.location.href = "/waiting";
   }; // 로그아웃 기능
 
   return (
